a1d23b0c4342fa1691d1be3b8db31b6131cf2395
klutzer <erico.klutzer@gmail.com>
2022-08-12 12:03:52 -0300

    Add types to handler function

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const redirectTo = (location: string): any => ({
+export const redirectTo = (location: string) => ({
   isBase64Encoded: false,
   statusCode: 302,
   multiValueHeaders: {
