9b55ee6f039a1efb170e9b5bea0ad0aaf5de7ea4
Mathaus Erich Ulbrich <mathulbrich@gmail.com>
2022-12-08 09:13:32 -0300

    Add queue-resolver option with DLQ

--- test/queue/application/queue-resolver.spec.ts
+++ test/queue/application/queue-resolver.spec.ts
@@ -1,18 +1,27 @@
+import { faker } from "@faker-js/faker";
+import { z } from "zod";
+
+import { Uuid } from "@app/lib/uuid";
 import { QueueResolver } from "@app/queue/application/queue-resolver";
+import { DummyEnqueuer } from "@test/resources/dummy-enqueuer";
 import { InMemoryPublisher } from "@test/resources/in-memory-publisher";
 
 class TestArguments {
   readonly publisher = new InMemoryPublisher();
-  readonly subscriber = this.publisher;
-  readonly resolver = new QueueResolver(this.subscriber);
+  readonly enqueuer = new DummyEnqueuer();
+  readonly resolver = new QueueResolver(this.publisher, this.enqueuer);
 }
 
-class ResolvedEvent {}
+const QueuePayload = z.object({ foo: z.string() });
+
+class ResolvedEvent {
+  constructor(readonly foo?: string) {}
+}
 class ResolvedEvent2 {}
 
 describe(QueueResolver.name, () => {
   it("should resolve when event is published", async () => {
-    const { subscriber, publisher, resolver } = new TestArguments();
+    const { publisher, resolver } = new TestArguments();
 
     await resolver.resolve({
       execute: async () => publisher.publish(new ResolvedEvent()),
@@ -20,13 +29,13 @@ describe(QueueResolver.name, () => {
       rejects: [],
     });
 
-    subscriber.singleOfType(ResolvedEvent);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent)).toBe(0);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent2)).toBe(0);
+    publisher.singleOfType(ResolvedEvent);
+    expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+    expect(publisher.totalOfListenersFor(ResolvedEvent2)).toBe(0);
   });
 
   it("should resolve when any of expected event is published", async () => {
-    const { subscriber, publisher, resolver } = new TestArguments();
+    const { publisher, resolver } = new TestArguments();
 
     await resolver.resolve({
       execute: async () => publisher.publish(new ResolvedEvent2()),
@@ -34,13 +43,13 @@ describe(QueueResolver.name, () => {
       rejects: [],
     });
 
-    subscriber.singleOfType(ResolvedEvent2);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent)).toBe(0);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent2)).toBe(0);
+    publisher.singleOfType(ResolvedEvent2);
+    expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+    expect(publisher.totalOfListenersFor(ResolvedEvent2)).toBe(0);
   });
 
   it("should not resolve when the published event is not the expected one", async () => {
-    const { subscriber, publisher, resolver } = new TestArguments();
+    const { publisher, resolver } = new TestArguments();
 
     await expect(
       resolver.resolve({
@@ -50,13 +59,13 @@ describe(QueueResolver.name, () => {
       }),
     ).rejects.toThrow("Queue execution resolves or rejects conditions were not met");
 
-    subscriber.singleOfType(ResolvedEvent);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent)).toBe(0);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent2)).toBe(0);
+    publisher.singleOfType(ResolvedEvent);
+    expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+    expect(publisher.totalOfListenersFor(ResolvedEvent2)).toBe(0);
   });
 
   it("should not resolve when no events are expected", async () => {
-    const { subscriber, publisher, resolver } = new TestArguments();
+    const { publisher, resolver } = new TestArguments();
 
     await expect(
       resolver.resolve({
@@ -69,13 +78,13 @@ describe(QueueResolver.name, () => {
       }),
     ).rejects.toThrow("Queue execution resolves or rejects conditions were not met");
 
-    subscriber.ofTypes(ResolvedEvent, ResolvedEvent2);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent)).toBe(0);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent2)).toBe(0);
+    publisher.ofTypes(ResolvedEvent, ResolvedEvent2);
+    expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+    expect(publisher.totalOfListenersFor(ResolvedEvent2)).toBe(0);
   });
 
   it("should not resolve when rejected event is published", async () => {
-    const { subscriber, publisher, resolver } = new TestArguments();
+    const { publisher, resolver } = new TestArguments();
 
     await expect(
       resolver.resolve({
@@ -85,13 +94,13 @@ describe(QueueResolver.name, () => {
       }),
     ).rejects.toThrow("Queue execution resolves or rejects conditions were not met");
 
-    subscriber.singleOfType(ResolvedEvent);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent)).toBe(0);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent2)).toBe(0);
+    publisher.singleOfType(ResolvedEvent);
+    expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+    expect(publisher.totalOfListenersFor(ResolvedEvent2)).toBe(0);
   });
 
   it("should not resolve when any of rejected event is published", async () => {
-    const { subscriber, publisher, resolver } = new TestArguments();
+    const { publisher, resolver } = new TestArguments();
 
     await expect(
       resolver.resolve({
@@ -101,13 +110,13 @@ describe(QueueResolver.name, () => {
       }),
     ).rejects.toThrow("Queue execution resolves or rejects conditions were not met");
 
-    subscriber.singleOfType(ResolvedEvent2);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent)).toBe(0);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent2)).toBe(0);
+    publisher.singleOfType(ResolvedEvent2);
+    expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+    expect(publisher.totalOfListenersFor(ResolvedEvent2)).toBe(0);
   });
 
   it("should not reject when the published event is not the expected one", async () => {
-    const { subscriber, publisher, resolver } = new TestArguments();
+    const { publisher, resolver } = new TestArguments();
 
     await resolver.resolve({
       execute: async () => publisher.publish(new ResolvedEvent2()),
@@ -115,8 +124,109 @@ describe(QueueResolver.name, () => {
       rejects: [ResolvedEvent],
     });
 
-    subscriber.singleOfType(ResolvedEvent2);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent)).toBe(0);
-    expect(subscriber.totalOfListenersFor(ResolvedEvent2)).toBe(0);
+    publisher.singleOfType(ResolvedEvent2);
+    expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+    expect(publisher.totalOfListenersFor(ResolvedEvent2)).toBe(0);
+  });
+
+  describe("#resolveWith", () => {
+    it("should resolve with data without publish to DLQ", async () => {
+      const { enqueuer, publisher, resolver } = new TestArguments();
+
+      await resolver.resolveWith({
+        execute: async (payload) => publisher.publish(new ResolvedEvent(payload.foo)),
+        resolves: [ResolvedEvent],
+        rejects: [],
+        data: {
+          foo: "bar",
+        },
+        parser: QueuePayload,
+      });
+
+      const published = publisher.singleOfType(ResolvedEvent);
+      expect(published.foo).toBe("bar");
+      expect(enqueuer.getEnqueuedMessages()).toHaveLength(0);
+    });
+
+    it("should resolve with invalid data without publish to DLQ", async () => {
+      const { enqueuer, publisher, resolver } = new TestArguments();
+
+      await resolver.resolveWith({
+        execute: async (payload) => publisher.publish(new ResolvedEvent(payload.foo)),
+        resolves: [ResolvedEvent],
+        rejects: [],
+        data: {
+          foo: 123,
+        },
+        parser: QueuePayload,
+      });
+
+      expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+      expect(enqueuer.getEnqueuedMessages()).toHaveLength(0);
+    });
+
+    it("should resolve with invalid data and publish to DLQ", async () => {
+      const { enqueuer, publisher, resolver } = new TestArguments();
+      const group = Uuid.generate().toString();
+      const messageId = Uuid.generate().toString();
+
+      await resolver.resolveWith({
+        execute: async (payload) => publisher.publish(new ResolvedEvent(payload.foo)),
+        resolves: [ResolvedEvent],
+        rejects: [],
+        data: {
+          foo: 123,
+        },
+        parser: QueuePayload,
+        dlq: {
+          name: "create-example",
+          group,
+          messageId,
+        },
+      });
+
+      expect(publisher.totalOfListenersFor(ResolvedEvent)).toBe(0);
+      expect(enqueuer.getEnqueuedMessages()).toHaveLength(1);
+      const enqueuedMessage = enqueuer.getEnqueuedMessages()[0];
+      expect(enqueuedMessage.payload).toBe(`{"foo":123}`);
+      expect(enqueuedMessage.queue).toBe("create-example");
+      expect(enqueuedMessage.messageId).toBe(messageId);
+      expect(enqueuedMessage.groupId).toBe(group);
+    });
+
+    it("should throw and not send to DLQ on execute error", async () => {
+      const { enqueuer, resolver } = new TestArguments();
+
+      await expect(
+        resolver.resolveWith({
+          execute: async () => {
+            throw new Error("execute error");
+          },
+          resolves: [],
+          rejects: [],
+          data: { foo: "bar" },
+          parser: QueuePayload,
+          dlq: {
+            name: "create-example",
+          },
+        }),
+      ).rejects.toThrowError("execute error");
+
+      expect(enqueuer.getEnqueuedMessages()).toHaveLength(0);
+    });
+
+    it("should resolve with invalid payload and not send to DLQ when it's not provided", async () => {
+      const { enqueuer, resolver } = new TestArguments();
+
+      await resolver.resolveWith({
+        execute: async () => {},
+        resolves: [],
+        rejects: [],
+        data: { foo: faker.datatype.number() },
+        parser: QueuePayload,
+      });
+
+      expect(enqueuer.getEnqueuedMessages()).toHaveLength(0);
+    });
   });
 });
